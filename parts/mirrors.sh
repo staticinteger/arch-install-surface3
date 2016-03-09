599bffed2cb93e0780bd2dc1a1c8d3f5b8c04937
staticinteger <timothyjamespeacock@gmail.com>
2016-03-09 01:58:07 -0500

    Now only use US mirrors

--- parts/mirrors.sh
+++ parts/mirrors.sh
@@ -1,6 +1,5 @@
 # Configure our mirrors
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-
+curl -o /etc/pacman.d/mirrorlist.backup "https://www.archlinux.org/mirrorlist/?country=US"
 sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
 pacman -Syyu
