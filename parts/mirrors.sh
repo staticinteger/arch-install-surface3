59e64bf4adecfac12bc860148158136e304e2c0b
staticinteger <timothyjamespeacock@gmail.com>
2016-03-09 02:10:35 -0500

    Only update the package lists, not the packages themselves

--- parts/mirrors.sh
+++ parts/mirrors.sh
@@ -2,4 +2,4 @@
 curl -o /etc/pacman.d/mirrorlist.backup "https://www.archlinux.org/mirrorlist/?country=US"
 sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
-pacman -Syyu
+pacman -Sy
